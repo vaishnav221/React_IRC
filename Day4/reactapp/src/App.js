0155c1f803acfafc5900c69790fb07947cea9c5d
VAISHNAV S R <vaishnavsr221@gmail.com>
2023-12-13 11:34:53 +0530

    Day4

---
+++ Day4/reactapp/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import StateEx from './Components/StateEx';
+function App() {
+  return (
+    <div className="App">
+      <StateEx/>
+    </div>
+  );
+}
+
+export default App;
